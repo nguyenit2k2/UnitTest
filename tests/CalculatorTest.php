068d50a3091ee796ecb2a7e6f1b6812ea0d3a4ea
nguyenit2k2 <nntn.1911.o.o@gmail.com>
2022-07-10 18:42:20 +0700

    update php unit

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+class CalculatorTest extends \PHPUnit\Framework\TestCase{
+    public function testAdd() {
+
+        $calculator = new App\Calculator;
+        $result = $calculator->add(20,5);
+
+        $this->assertEquals(25,$result);
+    }
+    public function testSubtract() {
+
+        $calculator = new App\Calculator;
+        $result = $calculator->subtract(20,5);
+
+        $this->assertEquals(15,$result);
+    }
+    public function testMultiply(){
+
+        $calculator = new App\Calculator;
+        $result = $calculator->multiply(2,5);
+
+        $this->assertEquals(10,$result);
+    }
+    public function testDivide(){
+
+        $calculator = new App\Calculator;
+        $result = $calculator->divide(10,5);
+
+        $this->assertEquals(2,$result);
+    }
+}
